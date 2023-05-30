a4c55a1357176a0bb8358f6f3811283005a8b5fb
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-05-30 15:55:06 +0200

    added frontend componentst

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,27 @@
-
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
-    <div className="text-green-500">Hello Spotify</div>
-  )
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-4xl font-semibold">Bonjour</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+          <ListItem
+            image="/images/liked.png"
+            name="Titres Likés"
+            href="liked"
+          />
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-3xl font-semibold">Nouveautés</h1>
+        </div>
+        <div>Liste des titres</div>
+      </div>
+    </div>
+  );
 }
